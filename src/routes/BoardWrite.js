676a8b0757ef1008506ca215b8e3824ea59f06fd
CHAEDOGYUNG <host0320@naver.com>
2024-06-26 16:47:51 +0900

    first commit

---
+++ src/routes/BoardWrite.js
@@ -0,0 +1,69 @@
+import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+import axios from "axios";
+
+const BoardWrite = () => {
+    const navigate = useNavigate();
+    const [board, setBoard] = useState({
+        title: '',
+        createdBy: '',
+        contents: '',
+    })
+
+    const {title, createdBy, contents} = board;
+
+    const onChange = (event) => {
+        const { value, name } = event.target; //event.target에서 name과 value만 가져오기
+        setBoard({
+            ...board,
+            [name]: value,
+        });
+    };
+
+    const saveBoard = async () => {
+        await axios.post(`//localhost:8082/board`, board).then((res) => {
+            alert('등록되었습니다.');
+            navigate('/board');
+        });
+    };
+
+    const backToList = () => {
+        navigate('/board');
+    };
+
+    return (
+        <div>
+            <div>
+                <span>제목</span>
+                <input type="text" name="title" value={title} onChange={onChange}/>
+            </div>
+            <br/>
+            <div>
+                <span>작성자</span>
+                <input type="text"
+                       name="createdBy"
+                       value={createdBy}
+                       onChange={onChange}
+                />
+            </div>
+            <br/>
+            <div>
+             <span>내용</span>
+                <textarea
+                name="contents"
+                cols="30"
+                rows="10"
+                value={contents}
+                onChange={onChange}
+                ></textarea>
+            </div>
+            <br/>
+            <div>
+                <button onClick={saveBoard}>저장</button>
+                <button onClick={backToList}>취소</button>
+            </div>
+        </div>
+    )
+}
+
+export default BoardWrite;
